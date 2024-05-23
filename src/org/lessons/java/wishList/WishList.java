d1ccd146dc21c65bcc1cbed8c52693c90a7b90b9
federico-casu <federico.casu1101@gmail.com>
2024-05-23 15:47:20 +0200

    feat: added feature to save the list by writing it to a file

--- src/org/lessons/java/wishList/WishList.java
+++ src/org/lessons/java/wishList/WishList.java
@@ -1,14 +1,17 @@
 package org.lessons.java.wishList;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class WishList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<Present> wishList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         int choice = -1;
+        FileWriter writer = new FileWriter("./resources/wishList.txt", true);
 
         while (choice != 0) {
             printMenu();
@@ -25,13 +28,21 @@ public class WishList {
                     // print wishList
                     printList(wishList);
                     break;
+                case 3:
+                    // save wishList
+                    try {
+                        writeListToFile(writer, wishList);
+                    } catch (IOException luisa) {
+                        System.out.println("Unable to save the wish list");
+                    }
+                    break;
                 case 0:
                     break;
                 default:
                     System.out.println("Scelta non valida");
             }
         }
-
+        writer.close();
         scanner.close();
     }
 
@@ -42,6 +53,7 @@ public class WishList {
         System.out.println("--------------------");
         System.out.println("1 - Inserisci nuovo regalo");
         System.out.println("2 - Stampa lista regali");
+        System.out.println("3 - Salva lista");
         System.out.println("0 - Esci");
     }
 
@@ -49,6 +61,14 @@ public class WishList {
         int counter = 1;
         for (Present present : list) {
             System.out.println("Present n." + counter + " " + present.getName());
+            counter++;
+        }
+    }
+
+    public static void writeListToFile(FileWriter fw, List<Present> list) throws IOException {
+        int counter = 1;
+        for (Present present : list) {
+            fw.write("\nPresent n." + counter + " " + present.getName());
             counter++;
         }
     }
